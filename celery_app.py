f7078962469a71cfea3b7e584827f2d82f984991
mroluwasesan <mr.oluwasesan@gmail.com>
2024-07-13 14:57:15 +0100

    celery

--- celery_app.py
+++ celery_app.py
@@ -2,11 +2,11 @@ from celery import Celery
 
 def make_celery(app):
     celery = Celery(
-    __name__,
+    app.import_name,
     backend='rpc://',  # Example: Using RPC as backend
     broker='pyamqp://guest@localhost//'  # Example: Using RabbitMQ as broker
-)
-
-def make_celery(app):
+    )
     celery.conf.update(app.config)
     return celery
+
+
